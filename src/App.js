1abaea7fe16410acd4fd70efc9fe035320737249
Iván Munguía <ivanalejandro249@gmail.com>
2019-11-04 13:11:07 -0600

    feat: Nebula with uber

--- src/App.js
+++ src/App.js
@@ -1,24 +1,65 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import DeckGL from "deck.gl";
+import { EditableGeoJsonLayer, DrawPolygonMode } from "nebula.gl";
+import MapGL from "react-map-gl";
+
+import "mapbox-gl/dist/mapbox-gl.css";
+
+import "./App.css";
+
+const myFeatureCollection = {
+  type: "FeatureCollection",
+  features: []
+};
+
+const selectedFeatureIndexes = [];
+
+const initialViewport = {
+  latitude: 37.773,
+  longitude: -122.481,
+  zoom: 15.5
+};
+
+const TOKEN =
+  "pk.eyJ1IjoiYWxlcGhyaSIsImEiOiJjamdwbHpycjIyZm45Mndud3AzamRibHpqIn0.ejAHwSGT6dcGxiDOrPCFLg";
+
+const initialViewState = {
+  longitude: -122.41669,
+  latitude: 37.7853,
+  zoom: 13,
+  pitch: 0,
+  bearing: 0
+};
 
 function App() {
+  const [data, setData] = useState(myFeatureCollection);
+  const [viewport, setViewport] = useState(initialViewport);
+
+  const layer = new EditableGeoJsonLayer({
+    id: "geojson-layer",
+    data,
+    mode: DrawPolygonMode,
+    selectedFeatureIndexes,
+    onEdit: ({ updatedData }) => {
+      setData(updatedData);
+    }
+  });
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <DeckGL
+        initialViewState={initialViewState}
+        controller={true}
+        layers={[layer]}
+      >
+        <MapGL
+          {...viewport}
+          width="100%"
+          height="100%"
+          onViewportChange={viewport => setViewport(viewport)}
+          mapboxApiAccessToken={TOKEN}
+        />
+      </DeckGL>
     </div>
   );
 }
